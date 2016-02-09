7356e5f1b29910ea07198a89dd7455f11c249340
George Campbell <georgecampbell@seattleacademy.org>
2016-02-09 09:48:21 -0800

    add 03_george.js

---
+++ 03_george.js
@@ -0,0 +1,12 @@
+port = process.argv[2];
+console.log("port",port);
+var net = require('net');
+console.log("net",net);
+var server= net.createServer(respond);
+function respond(socket){
+	d = new Date();
+	socket.end(d.toString());
+}
+
+console.log("Listening on port ", port);
+server.listen(port);
